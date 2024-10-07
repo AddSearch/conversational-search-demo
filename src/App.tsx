af069458209904516ce12eba86e01b438d999310
janstoff <jan.steinhoff@gmail.com>
2024-10-07 10:58:00 +0100

    update dependencies to latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,140 @@
-import { useState } from 'react';
-import ReactLogo from './assets/react.svg';
-import ViteLogo from './assets/vite.svg';
-import TypescriptLogo from './assets/typescript.svg';
-import TailwindLogo from './assets/tailwind.svg';
-import PrettierLogo from './assets/prettier.svg';
-import EslintLogo from './assets/eslint.svg';
+import React from 'react';
+import { AddSearchUiIntegration } from 'react-addsearch-ui';
+import 'react-addsearch-ui/dist/style.css';
 
-function App() {
-    const [count, setCount] = useState(0);
+const DEFAULT_INDEX = '2c32bf3eb06b30af5f8208481aea3e8b';
+
+const DemoIndexInput: React.FC<{ readonly setCurrentIndex: (index: string) => void }> = ({
+    setCurrentIndex
+}) => {
+    const [indexInputValue, setIndexInputValue] = React.useState<string>(DEFAULT_INDEX);
 
     return (
-        <>
-            <div className="mt-44 flex items-center justify-center">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img className="w-24 h-24" src={ViteLogo} alt="Vite logo" />
-                </a>
-                <a className="ml-12" href="https://react.dev" target="_blank">
-                    <img className="w-24 h-24" src={ReactLogo} alt="React logo" />
-                </a>
-            </div>
-            <div className="mt-14 flex items-center justify-center">
-                <a href="https://www.typescriptlang.org/" target="_blank">
-                    <img className="w-12 h-12" src={TypescriptLogo} alt="Typescript logo" />
-                </a>
-                <a className="ml-6" href="https://tailwindcss.com/" target="_blank">
-                    <img className="w-12 h-12" src={TailwindLogo} alt="Tailwind CSS logo" />
-                </a>
-                <a className="ml-6" href="https://prettier.io/" target="_blank">
-                    <img className="w-12 h-12" src={PrettierLogo} alt="Prettier logo" />
-                </a>
-                <a className="ml-6" href="https://eslint.org/" target="_blank">
-                    <img className="w-12 h-12" src={EslintLogo} alt="ESLint logo" />
-                </a>
-            </div>
-            <div className="mt-12 grid gap-y-8 w-full justify-center">
-                <div>
-                    <h1 className="text-center text-xl font-semibold">Vite + React</h1>
-                    <h2 className="mt-1 text-center font-semibold">
-                        with Typescript, Tailwind CSS, Prettier and ESLint
-                    </h2>
-                </div>
-                <div className="flex flex-col items-center">
-                    <button
-                        className="w-full border border-slate-400 shadow-sm rounded-full px-2 py-1"
-                        onClick={() => setCount((count) => count + 1)}
-                    >
-                        count is {count}
-                    </button>
-                    <p className="mt-2">
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="text-slate-400 text-center">Click on the logos to learn more.</p>
-            </div>
-        </>
+        <div className="text-white flex items-center gap-2">
+            <span>Currently used index: </span>
+            <input
+                type="text"
+                value={indexInputValue}
+                onChange={(e) => setIndexInputValue(e.target.value)}
+                className="text-gray-800 border border-gray-300 rounded-md px-4 py-2"
+            />
+            <button
+                onClick={() => setCurrentIndex(indexInputValue)}
+                className="rounded-md border-2 border-white text-white px-4 py-2"
+            >
+                Apply Index
+            </button>
+        </div>
     );
-}
+};
+
+export const App: React.FC = () => {
+    const [currentIndex, setCurrentIndex] = React.useState<string>(DEFAULT_INDEX);
+
+    return (
+        <div>
+            <div className="flex flex-col gap-4 p-4 border-b border-gray-300 bg-[#fd564d] mb-6">
+                <h1 className="text-white text-3xl font-bold">
+                    AddSearch Demo (incl. Conversational Search)
+                </h1>
+                <DemoIndexInput setCurrentIndex={setCurrentIndex} />
+            </div>
+            <div>
+                <AddSearchUiIntegration
+                    key={currentIndex}
+                    publicSiteKey={currentIndex}
+                    primaryColor="#fd564d"
+                    customFont="Poppins"
+                    searchFieldHeaderText="Search"
+                    searchButtonText="Search"
+                    searchInputPlaceholderText="What are you looking for?"
+                    hasSearchInputAutofocus={true}
+                    hasSearchAsYouType={false}
+                    minLengthToShowSearchAsYouTypeResults={1}
+                    hasSearchInputFieldIcon={true}
+                    hasAutocomplete={true}
+                    hasConversationalSearch={true}
+                    loadMoreResults="button"
+                    filters={[
+                        {
+                            labelText: 'Categories',
+                            type: 'checkbox-group',
+                            options: {
+                                pricing: {
+                                    label: 'Pricing',
+                                    filter: { category: '1xpricing' }
+                                },
+                                partners: {
+                                    label: 'Partners',
+                                    filter: { category: '1xpartners' }
+                                },
+                                customers: {
+                                    label: 'Customers',
+                                    filter: { category: '1xcustomers' }
+                                }
+                            }
+                        },
+                        {
+                            // labelText: 'Documents',
+                            type: 'tags',
+                            options: {
+                                nofilter: {
+                                    label: 'All results'
+                                },
+                                blog: {
+                                    label: 'Blog',
+                                    filter: { category: '1xblog' }
+                                },
+                                docs: {
+                                    label: 'Documentation',
+                                    filter: { category: '1xdocs' }
+                                }
+                            }
+                        }
+                        // {
+                        //     type: 'select-list',
+                        //     options: {
+                        //         nofilter: {
+                        //             label: 'All results'
+                        //         },
+                        //         blog: {
+                        //             label: 'Blog',
+                        //             filter: { category: '1xblog' }
+                        //         },
+                        //         docs: {
+                        //             label: 'Documentation',
+                        //             filter: { category: '1xdocs' }
+                        //         }
+                        //     }
+                        // }
+                    ]}
+                    hasActiveFilters={true}
+                    sortByOptions={[
+                        {
+                            label: 'Most relevant first',
+                            sortBy: 'relevance',
+                            order: 'desc'
+                        },
+                        {
+                            label: 'Date: Newest first',
+                            sortBy: 'date',
+                            order: 'desc'
+                        },
+                        {
+                            label: 'Date: Oldest first',
+                            sortBy: 'date',
+                            order: 'asc'
+                        },
+                        {
+                            label: 'Multiple sorting parameters',
+                            sortBy: ['custom_fields.plan_name', 'date'],
+                            order: ['desc', 'desc']
+                        }
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
 
 export default App;
